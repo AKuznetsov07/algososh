a28cf1143f27f40ae01504fa540c5eb4069cad09
AKuznetsov <artemkuznetsov07@gmail.com>
2023-12-10 21:18:24 +0300

    queue

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,10 +1,106 @@
-import React from "react";
+import React, { ChangeEvent, MouseEvent, useEffect } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import styles from "./queue-page.module.css";
+import { Queue } from "../../utils/linearCollection";
+import { Circle, CircleProps } from "../ui/circle/circle";
+import { ElementStates } from "../../types/element-states";
+import { Button } from "../ui/button/button";
+import { v4 as uuidv4 } from "uuid";
 
 export const QueuePage: React.FC = () => {
-  return (
-    <SolutionLayout title="Очередь">
+    const wait = (delay: number, ...args: any[]) => new Promise(resolve => setTimeout(resolve, delay, ...args));
+    //let stringStack: Stack<string> = new Stack<string>();
+    const [value, setValue] = React.useState<{
+        inputStr: string,
+        stringStack: Queue<string>,
+        stringCirclesPropsList: Array<CircleProps>
+    }>({
+        inputStr: "",
+        stringStack: new Queue<string>(7),
+        stringCirclesPropsList: []
+    });
+    useEffect(() => {
+        drawCircles(false);
+    },[])
 
+    const onValueChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue({ ...value, inputStr: e.target.value });
+    };
+    const handleEnqueueClick = async (event: MouseEvent<HTMLButtonElement>) => {
+        //setValue({ ...value, inputStr: "" });
+        let tailIndex = value.stringStack.getTail();
+        if (tailIndex !== null) {
+            tailIndex++;
+        }
+        else {
+            tailIndex = 0;
+        }
+
+        drawCircles(true, tailIndex);
+        value.stringStack.enqueue(value.inputStr);
+        await wait(500);
+        drawCircles(false);
+    };
+    const handleDequeueClick = async (event: MouseEvent<HTMLButtonElement>) => {
+        //setValue({ ...value, inputStr: "" });
+        let headIndex = value.stringStack.getHead();
+        if (headIndex===null) {
+            headIndex = 0;
+        }
+        drawCircles(true, headIndex);
+        value.stringStack.dequeue();
+        await wait(500);
+        drawCircles(false);
+    };
+    const handleClearClick = async (event: MouseEvent<HTMLButtonElement>) => {
+        value.stringStack = new Queue<string>(7);
+        drawCircles(false);
+    };
+
+    function drawCircles(isColored: boolean, colorIndex:number = 0) {
+        const values = value.stringStack.getVisualisationData();
+        if (values.length === 0) {
+            return setValue({ ...value, stringCirclesPropsList: [], inputStr: "" });
+        }
+        const result: Array<CircleProps> = [];
+        for (let i = 0; i < values.length; i++) {
+            const letterValue = values[i] !== null ? values[i] : "" as string;
+            result.push({
+                letter: letterValue as string,
+                index: i
+            });
+        }
+
+        if (isColored) {
+            result[colorIndex].state = ElementStates.Changing;
+        }
+
+        const headIndex = value.stringStack.getHead();
+        if (headIndex !== null) {
+            result[headIndex].head = "head";
+        }
+        const tailIndex = value.stringStack.getTail();
+        if (tailIndex) {
+            result[tailIndex].tail = "tail";
+        }
+        setValue({ ...value, stringCirclesPropsList: result, inputStr: "" });
+    }
+
+
+  return (
+      <SolutionLayout title="Очередь">
+          <div className={`${styles.inputRow}`}>
+              <Input maxLength={4} isLimitText={true} value={value.inputStr} onChange={onValueChange}></Input>
+              <Button text="add" onClick={handleEnqueueClick}></Button>
+              <Button text="drop" onClick={handleDequeueClick}></Button>
+              <Button text="clear" onClick={handleClearClick}></Button>
+          </div>
+          <div className={`${styles.circlesGrid}`}>
+              {value.stringCirclesPropsList.map((circlesProps) => (
+                  <Circle key={uuidv4()} {...circlesProps} />
+              ))}
+          </div>
     </SolutionLayout>
   );
 };
--- src/utils/linearCollection.ts
+++ src/utils/linearCollection.ts
@@ -24,3 +24,60 @@ export class Stack<T> {
     getSize = () => this.container.length;
     getVisualisationData = () => [...this.container]
 }
+export class Queue<T> {
+    private container: (T | null)[] = [];
+    private _head = 0;
+    private _tail = 0;
+    private readonly size: number = 0;
+    private length: number = 0;
+
+    constructor(size: number) {
+        this.size = size;
+        this.container = Array(size);
+    }
+
+    enqueue = (item: T) => {
+        if (this.length >= this.size) {
+            throw new Error("Maximum length exceeded");
+        }
+        this.container[this._tail % this.size] = item;
+        this._tail++;
+        this.length++;
+    };
+
+    dequeue = () => {
+        if (this.isEmpty()) {
+            throw new Error("No elements in the queue");
+        }
+        const item = this.container[this._head]
+        delete this.container[this._head]
+        this._head++;
+        this.length = this.length - 1;
+    };
+
+    peak = (): T | null => {
+        if (this.isEmpty()) {
+            throw new Error("No elements in the queue");
+        }
+        return this.container[this._head];
+    };
+
+    isEmpty = () => this.length === 0;
+    getVisualisationData = () => [...this.container]
+    getTail = () => {
+        if (this.length === 0) {
+            return null;
+        }
+        else {
+            return this._tail - 1;
+        }
+    }
+    getHead = () => {
+        if (this.length === 0) {
+            return null;
+        }
+        else {
+            return this._head;
+        }
+    }
+}
